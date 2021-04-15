2f75c42535cab5bf8f7f7325476ff5fd4ed055ff
Your Name <you@example.com>
2021-04-15 10:23:57 +0200

    library size for different percentiles

--- annotation_process/libsize_generate.py
+++ annotation_process/libsize_generate.py
@@ -2,14 +2,19 @@ import pandas as pd
 import h5py 
 import numpy as np 
 import logging
+import os 
 
-
-def compute_library_size(path_expr, path_gene_list, path_save, percentile ):
+def compute_library_size(path_expr, path_gene_list, path_save, percentile, query_no_version = False ):
     logging.info("Path of graph with gene expressions:  {}".format(path_expr))
     h5_gene_expr = h5py.File(open(path_expr, 'rb'))
     coding_list = pd.read_csv(path_gene_list, header = None, names = ['genes'])
     logging.info("Querying coding genes")
-    h5_coding_mx = [list(h5_gene_expr["raw_count"][gene_id, :])
+    if query_no_version: 
+        h5_coding_mx = [list(h5_gene_expr["raw_count"][gene_id, :])
+                  for gene_id, gene_name in enumerate(h5_gene_expr['gene_ids'])
+                  if gene_name.decode().split('.')[0] in coding_list['genes'].values]
+    else:
+        h5_coding_mx = [list(h5_gene_expr["raw_count"][gene_id, :])
                   for gene_id, gene_name in enumerate(h5_gene_expr['gene_ids'])
                   if gene_name.decode() in coding_list['genes'].values]
     h5_coding_mx = np.array(h5_coding_mx)
@@ -17,29 +22,43 @@ def compute_library_size(path_expr, path_gene_list, path_save, percentile ):
     logging.info("Number of coding genes is: {}".format(coding_list.shape))
     sample_names = [strain.decode() for strain in h5_gene_expr['strains']]
     logging.info("Computing library percentiles")
-    libsize_count = pd.DataFrame({'sample': sample_names,
-                                 'libsize_75percent': np.percentile(h5_coding_mx, percentile, axis = 0 )})
-    logging.info("Saving")
+    libsize_count = pd.DataFrame({'sample': sample_names})
+    for perc in percentile:
+        libsize_count['libsize_{}percent'.format(perc)] =  np.percentile(h5_coding_mx, perc, axis = 0 )
+    logging.info("Saving to {}".format(path_save))
     libsize_count.to_csv(path_save, index = None, sep = '\t')
 
-percentile=75
+percentile=[ 50, 60, 70, 75, 80, 90, 95 ]  
+outdir = '/cluster/work/grlab/projects/TCGA/PanCanAtlas/immunopepper_paper/peptides_ccell_rerun_gtex_151220'
+
+path_expr = '/cluster/work/grlab/projects/GTEx/rna/results/splicing/spladder/genes_graph_conf2.merge_graphs.count.gene_expression.hdf5' # V30 
+path_gene_list = '/cluster/work/grlab/projects/projects2020_OHSU/gene_lists/genes_coding_gencode_v32_inter_v30_wo_version.txt'
+tag = ".coding_gencode_v32_inter_v30_wo_version.txt"
+path_save = os.path.join(outdir, 'libsize{}.tsv'.format(tag)) 
+query_no_version = True 
+compute_library_size(path_expr, path_gene_list, path_save, percentile, query_no_version)
+
+
 path_expr = '/cluster/work/grlab/projects/GTEx/rna/results/splicing/spladder/genes_graph_conf2.merge_graphs.count.gene_expression.hdf5'
 path_gene_list = '/cluster/work/grlab/projects/projects2020_OHSU/gene_lists/genes_coding_gencode_v32_inter_v30.txt'
-path_save = '/cluster/work/grlab/projects/TCGA/PanCanAtlas/immunopepper_paper/peptides_ccell_rerun_gtex_151220/GTEX2019_commit_1fc5828_pya.0.17.1_ref_mode2'
 tag = ".coding_gencode_v32_inter_v30.txt"
-path_save = 'expression_counts.libsize{}.tsv'.format(tag)
+path_save = path_save = os.path.join(outdir, 'libsize{}.tsv'.format(tag)) 
 compute_library_size(path_expr, path_gene_list, path_save, percentile )
 
 path_expr = '/cluster/work/grlab/projects/GTEx/rna/results/splicing/spladder/genes_graph_conf2.merge_graphs.count.gene_expression.hdf5'
 path_gene_list = '/cluster/work/grlab/projects/projects2020_OHSU/gene_lists/genes_coding_gencode_v30.txt'
-path_save = '/cluster/work/grlab/projects/TCGA/PanCanAtlas/immunopepper_paper/peptides_ccell_rerun_gtex_151220/GTEX2019_commit_1fc5828_pya.0.17.1_ref_mode2'
 tag = ".coding_gencode_v30.txt"
-path_save = 'expression_counts.libsize{}.tsv'.format(tag)
+path_save = path_save = os.path.join(outdir, 'libsize{}.tsv'.format(tag))
+compute_library_size(path_expr, path_gene_list, path_save, percentile )
+
+path_expr = '/cluster/work/grlab/projects/GTEx/rna/results/splicing/spladder/genes_graph_conf2.merge_graphs.count.gene_expression.hdf5'
+path_gene_list = '/cluster/work/grlab/projects/projects2020_OHSU/gene_lists/genes_coding_gencode_v32.txt'
+tag = ".coding_gencode_v32.txt"
+path_save = path_save = os.path.join(outdir, 'libsize{}.tsv'.format(tag))
 compute_library_size(path_expr, path_gene_list, path_save, percentile )
 
 path_expr = '/cluster/work/grlab/projects/TCGA/PanCancer/rerun2018_alt_splice_GTEx/spladder/genes_graph_conf2.merge_graphs.validated.count.gene_expression.hdf5'
 path_gene_list = '/cluster/work/grlab/projects/TCGA/PanCanAtlas/immunopepper_paper/peptides_ccell_rerun_gtex_151220/coding_genes_nocap_GTEX2017'
-path_save = '/cluster/work/grlab/projects/TCGA/PanCanAtlas/immunopepper_paper/peptides_ccell_rerun_gtex_151220/GTEX2017_commit_1fc5828_pya.0.17.1_ref'
 tag = ".coding_gencode_v19.txt"
-path_save = 'expression_counts.libsize{}.tsv'.format(tag)
+path_save = path_save = os.path.join(outdir, 'libsize{}.tsv'.format(tag)) 
 compute_library_size(path_expr, path_gene_list, path_save, percentile )
--- annotation_process/send_libsize_generate.sh
+++ annotation_process/send_libsize_generate.sh
@@ -7,7 +7,7 @@ parallel=1
 
 ### Immunopepper Run
 
-cmd='python /cluster/home/prelotla/GitHub/projects2020_ohsu/annotation_process/libsize_generate.py' 
+cmd='python /cluster/home/prelotla/GitHub/projects2020_ohsu/annotation_process/libsize_generate.py > /cluster/work/grlab/projects/TCGA/PanCanAtlas/immunopepper_paper/peptides_ccell_rerun_gtex_151220/GTEX2019_commit_1fc5828_pya.0.17.1_ref_mode2/regenerate_lib.log 2>&1' 
 
 echo $cmd
 echo $cmd | bsub -n ${parallel} -J libsize -W ${time_}:00 -R "rusage[mem=${mem}]" -R "span[hosts=1]"  #-o $logfile #-e ${logfile}.e -o $logfile #-R "span[hosts=1]" -o $logfile
