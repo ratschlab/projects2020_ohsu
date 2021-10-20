7eae4d32c1c70beaca43afbc5db8f1208f37b69e
Your Name <laurie.prelot@inf.ethz.ch>
2021-10-20 16:39:52 +0200

    ohsu filt oct

--- peptides_filtering/deal_uncomplete_gene_process.sh
+++ peptides_filtering/deal_uncomplete_gene_process.sh
@@ -1,32 +1,41 @@
 #!/bin/bash
 set -e
 
-suffix_out='GTEX2019'
+suffix_out='Normals'
 if [ $suffix_out == 'GTEX2019' ]; then 
 	outfolder=/cluster/work/grlab/projects/TCGA/PanCanAtlas/immunopepper_paper/peptides_ccell_rerun_gtex_151220/GTEX2019_commit_v2.5f0752a_pya.0.17.1_conf2_annot_ref_chrall_cap/cohort_mutNone
 elif  [ $suffix_out == 'BRCA' ]; then 
 	outfolder=/cluster/work/grlab/projects/projects2020_OHSU/peptides_generation/v2_v2.5f0752a_conf2_annotFrame_cap0_runs_pya0.17.1/TCGA_Breast_1102/cohort_mutNone
+elif [ $suffix_out == 'Normals' ]; then
+	outfolder=/cluster/work/grlab/projects/projects2020_OHSU/peptides_generation/v2_v2.5f0752a_conf2_annotFrame_cap0_runs_pya0.17.1/TCGA_All_Normals/cohort_mutNone
 fi 
 
 gene_interest_file=./dev_genes
 #gene_interest_file=./tmp_genes
 
-file_out=Segm
-if [ ${file_out} == 'Junc' ]; then 
-	file_out_full=ref_graph_kmer_JuncExpr.pq
-elif  [ ${file_out} == 'Segm' ]; then
-	file_out_full=ref_graph_kmer_SegmExpr.pq
-elif [ ${file_out} == 'annot' ] ; then 
-	file_out_full=ref_annot_9mer.pq
-fi
 
-run_status='get' #'collect_finished'
+run_status='collect_finished' #'collect_finished'
 
 if [ $run_status == 'collect_finished' ] ; then  
 	echo "collect processed genes to $(dirname ${outfolder})/ref_gene_folder_eq"
-	for folder in ${outfolder}/tmp*/gene_expression_detail.pq ; do echo $(dirname $folder)  $(parquet-tools csv -c gene $folder  | cut -f1 -d ',' | grep ENS) ;  done > $(dirname ${outfolder})/ref_gene_folder_eq	
-else
+	for folder in ${outfolder}/tmp*/gene_expression_detail.pq ; 
+		do echo $(dirname $folder)  $(parquet-tools csv -c gene $folder  | cut -f1 -d ',' | grep ENS) > $(dirname ${outfolder})/ref_gene_folder_eq	
+	done
+	#TODO add check for folders without gene output 
+	else
+		file_out=Segm
+		if [ ${file_out} == 'Junc' ]; then
+		        file_out_full=ref_graph_kmer_JuncExpr.pq
+		elif  [ ${file_out} == 'Segm' ]; then
+       	 		file_out_full=ref_graph_kmer_SegmExpr.pq
+		elif [ ${file_out} == 'annot' ] ; then
+        		file_out_full=ref_annot_9mer.pq
+		fi
 	while read gene; do
-		 grep -E "${gene}" $(dirname ${outfolder})/ref_gene_folder_eq | sed -z 's/\n/|/g;s/|$/\n/' | cut -f1 -d ' ' | sed "s,$,/${file_out_full},g" >> ./tmp20211015_input_${file_out}_${suffix_out}
+		myfile=$(grep -E "${gene}" $(dirname ${outfolder})/ref_gene_folder_eq | sed -z 's/\n/|/g;s/|$/\n/' | cut -f1 -d ' ' | sed "s,$,/${file_out_full},g")
+	       	if [ -f "${myfile}" ]; then
+			echo $myfile >> ./tmp20211015_input_${file_out}_${suffix_out}
+		fi
  	done < ${gene_interest_file} 
+
 fi 
--- peptides_filtering/send_all_samples_BRCA-GTEX2019.sh
+++ peptides_filtering/send_all_samples_BRCA-GTEX2019.sh
@@ -4,13 +4,11 @@ set -e
 
 ### Lsf and Run Parameters
 mem=20000
-time_=120
+time_=75
 local_=run_cluster #"run_local"
 parallel=6
 #edge_or_segm=edge
-suffix="commit83ac08b"
-file_tmp='20211015'
-#file_tmp=''
+suffix="commit_0a02cfe"
 echo "WARNING check activation myimmuno3"
 
 ### Inputs
@@ -37,49 +35,70 @@ else
 	target_cancer='' #TODO update target 
 	libsize_cancer=/cluster/work/grlab/projects/projects2020_OHSU/peptides_generation/v2_libsizes_conf2_annotFrame_cap1000_runs_pya0.17.1_KEEP/TCGA_Breast_1102/cohort_mutNone/TCGA_Breast_1102_coding_libsize75.tsv
 fi
+input_Junc_cancer=/cluster/work/grlab/projects/projects2020_OHSU/peptides_generation/v2_v2.5f0752a_conf2_annotFrame_cap0_runs_pya0.17.1/TCGA_Breast_1102/cohort_mutNone_relink/Junc_BRCA_19077
+input_annot_cancer=/cluster/work/grlab/projects/projects2020_OHSU/peptides_generation/v2_v2.5f0752a_conf2_annotFrame_cap0_runs_pya0.17.1/TCGA_Breast_1102/cohort_mutNone_relink/annot_BRCA_19077
+input_Segm_normal=/cluster/work/grlab/projects/projects2020_OHSU/peptides_generation/v2_v2.5f0752a_conf2_annotFrame_cap0_runs_pya0.17.1/TCGA_Breast_1102/cohort_mutNone_relink/Segm_GTEX2019_19077
+input_Junc_normal=/cluster/work/grlab/projects/projects2020_OHSU/peptides_generation/v2_v2.5f0752a_conf2_annotFrame_cap0_runs_pya0.17.1/TCGA_Breast_1102/cohort_mutNone_relink/Junc_GTEX2019_19077
 
-#TODO Update right limit 
+normalizer_cancer_libsize='400000'
+normalizer_normal_libsize=${normalizer_cancer_libsize}
+kmer='9'
+
+parallelism='1000'
+out_partitions=1
+scratch_mem=155000
+test_=''
+
 cohort_expr_lim_cancer='1'
 expr_n_limit_cancer='2'
 sample_expr_lim_cancer='2' #already conf2 
-kmer='9'
 expr_n_limit_normal='3' 
 cohort_expr_lim_normal='1'
-normalizer_cancer_libsize='400000'
-normalizer_normal_libsize=${normalizer_cancer_libsize}
 
-parallelism='10000'
-out_partitions=1
-scratch_mem=155000
-test_='norm'
 
 log_dir=${base_cancer}/lsf
 mkdir -p ${log_dir}
 ### Main 
-while read sample; do
-	for mutation_canc in ref; do 
-		## Organize folders
-		
-		## Generate instructions
-		output_dir=${base_cancer}/filter_${sample}/${suffix}
-		mkdir -p ${output_dir}	
-		logfile=${log_dir}/${sample}.cancerspec.${suffix}.log
-		echo $logfile
-		# --path-cancer-matrix-segm PATH_CANCER_MATRIX_SEGM [PATH_CANCER_MATRIX_SEGM ...]
-                #        segment expression integrated matrix of kmers * samples for foreground
- # --path-cancer-matrix-edge
- 		#--path-normal-kmer-list $(sed -z 's/\n/ /g' ./tmp_input_annot_BRCA)
-		cmd="immunopepper cancerspecif --cores $parallel --mem-per-core $mem --kmer $kmer --expression-fields-c "segmentExpr" "junctionExpr" --path-cancer-matrix-edge $(sed -z 's/\n/ /g' ./tmp${file_tmp}_input_Junc_BRCA) --ids-cancer-samples "${sample}" --mut-cancer-samples ${mutation_canc} --whitelist-cancer ${whitelist_cancer} --path-cancer-libsize ${libsize_cancer} --normalizer-cancer-libsize ${normalizer_cancer_libsize} --whitelist-normal ${whiltelist_normal} --path-normal-libsize ${libsize_normal} --normalizer-normal-libsize ${normalizer_normal_libsize} --output-dir $output_dir --cohort-expr-support-cancer ${cohort_expr_lim_cancer} --n-samples-lim-cancer ${expr_n_limit_cancer} --sample-expr-support-cancer ${sample_expr_lim_cancer} --uniprot ${uniprot} --parallelism ${parallelism} --out-partitions ${out_partitions} --path-normal-matrix-segm $(sed -z 's/\n/ /g' ./tmp${file_tmp}_input_Segm_GTEX2019) --path-normal-matrix-edge  $(sed -z 's/\n/ /g' ./tmp${file_tmp}_input_Junc_GTEX2019) --path-normal-kmer-list $(sed -z 's/\n/ /g' ./tmp${file_tmp}_input_annot_BRCA) --cohort-expr-support-norm ${cohort_expr_lim_normal} --n-samples-lim-normal ${expr_n_limit_normal} --scratch-dir 'TMPDIR' > ${base_cancer}/${sample}.${mutation_canc}.run_cancerspecif.${suffix}${test_}.log 2>&1" 
-				
-		## Run
-		if [ "$local_" = "run_local" ] ; then
-			echo "running local"
-			echo $cmd
-			#$cmd
-		else
-			echo $cmd
-			echo $cmd | bsub -n ${parallel} -J Filtipp -W ${time_}:00 -R "rusage[mem=${mem}]" -R "span[hosts=1]" -R "rusage[scratch=$scratch_mem]" #-o $logfile #-e ${logfile}.e -o $logfile #-R "span[hosts=1]" -o $logfile
-		fi
+##TODO add argument core whitelist; all normal subset and all normals with whitelist
+for cohort_expr_lim_cancer in '1' ; do #'none' '5'; do 
+	for expr_n_limit_cancer in '1'; do #'2' '10'; do 
+		for sample_expr_lim_cancer in '2' ; do #'0' ; do #other type of splicing graph
+			for expr_n_limit_normal in '1' '2' '10'; do 
+				for cohort_expr_lim_normal in '0' '3' '10'; do  
+				   for background in 'gtex'; do 
+					while read sample; do
+						for mutation_canc in ref; do 
+							## Organize folders
+							
+							## Generate instructions
+							output_dir=${base_cancer}/filter_${sample}/${suffix}_${background}
+							mkdir -p ${output_dir}	
+							logfile=${log_dir}/${sample}.cancerspec.${suffix}.log
+							echo $logfile
+							
+							cmd="immunopepper cancerspecif --cores $parallel --mem-per-core $mem --kmer $kmer --expression-fields-c "segmentExpr" "junctionExpr" --path-cancer-matrix-edge ${input_Junc_cancer} --ids-cancer-samples "${sample}" --mut-cancer-samples ${mutation_canc} --whitelist-cancer ${whitelist_cancer} --path-cancer-libsize ${libsize_cancer} --normalizer-cancer-libsize ${normalizer_cancer_libsize} --whitelist-normal ${whiltelist_normal} --path-normal-libsize ${libsize_normal} --normalizer-normal-libsize ${normalizer_normal_libsize} --output-dir $output_dir --cohort-expr-support-cancer ${cohort_expr_lim_cancer} --n-samples-lim-cancer ${expr_n_limit_cancer} --sample-expr-support-cancer ${sample_expr_lim_cancer} --uniprot ${uniprot} --parallelism ${parallelism} --out-partitions ${out_partitions} --path-normal-matrix-segm ${input_Segm_normal} --path-normal-matrix-edge ${input_Junc_normal} --path-normal-kmer-list ${input_annot_cancer}" #TODO add back scratch for cancer? --scratch-dir 'TMPDIR'" 
+							
+							if [ ${cohort_expr_lim_cancer} != 'none' ]; then 	
+								cmd1="${cmd} --cohort-expr-support-norm ${cohort_expr_lim_normal} --n-samples-lim-normal ${expr_n_limit_normal}"
+							else
+								cmd1="${cmd}"	
+							fi 
+							cmd2="${cmd1} > ${base_cancer}/${sample}.${mutation_canc}.run_cancerspecif.${suffix}.Cec${cohort_expr_lim_cancer}.Cn${expr_n_limit_cancer}.Ces.${sample_expr_lim_cancer}.Nec${cohort_expr_lim_normal}.Nn${expr_n_limit_normal}.${test_}.log 2>&1"	
+							## Run
+							if [ "$local_" = "run_local" ] ; then
+								echo "running local"
+								echo $cmd2
+								#$cmd
+							else
+								echo $cmd2
+								echo $cmd2 | bsub -n ${parallel} -J Filtipp -W ${time_}:00 -R "rusage[mem=${mem}]" -R "span[hosts=1]" -R "rusage[scratch=$scratch_mem]" -o $logfile #-e ${logfile}.e -o $logfile #-R "span[hosts=1]" -o $logfile
+							fi
 
+						done
+					done < ./tmp_samples #/cluster/work/grlab/projects/projects2020_OHSU/sample_lists/TCGA_foreground/BRCA_5samples_spladder_full.csv #./tmp_samples
+				done
+			done
+		done
 	done
-done < /cluster/work/grlab/projects/projects2020_OHSU/sample_lists/TCGA_foreground/BRCA_5samples_spladder_full.csv #./tmp_samples
+done
+done
