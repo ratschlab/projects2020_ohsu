3089cc53ed6c255a88bfaab3cf339b2e9f817801
lthp <laurie.prelot@inf.ethz.ch>
2023-01-17 12:57:12 +0100

    update GTEX large runs scripts

--- eth/peptides_generation/foreground/send_all_cross_sample_TCGA_frames_grc38.sh
+++ eth/peptides_generation/foreground/send_all_cross_sample_TCGA_frames_grc38.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
 ### Run parameters
-mem=50G
+mem=110G
 time_=120
 local_=run_cluster 
-parallel=4 #8 $2
+parallel=2 #8 $2
 
 ### Immunopepper parameters
-start_id=0
+start_id=9000
 cap=0 #TODO 
 batch_size=1 $4
 frames=annot
@@ -17,7 +17,7 @@ basedir_splice=/cluster/work/grlab/projects/projects2021-immuno_peptides/results
 #basedir=/cluster/work/grlab_shared/TMP/prelotla/tcga
 #basedir_splice=/cluster/work/grlab_shared/TMP/prelotla/tcga/TCGA_for_neoepitopes
 base_path=${basedir}/peptides_generation/CANCER_eth
-coding_genes=/cluster/work/grlab/projects/projects2020_OHSU/gene_lists/OHSU_gencodev32_proteincodinggeneids.txt
+coding_genes=${basedir}/gene_lists/OHSU_gencodev32_proteincodinggeneids.txt
 
 ### Inputs
 annotation="${basedir}/annotation/gencode.v32.annotation.gtf"
@@ -25,12 +25,13 @@ genome="${basedir}/genome/GRCh38.p13.genome.fa"
 vcf_path="${basedir}/germline_variants/mergedfiles_clean_stringentfilter.matchIds.h5" # Dummy, see if we have the variant calls with the right genome
 maf_path="${basedir}/somatic_variants/pancan.merged.v0.2.6.PUBLIC.matchIds.maf" # Dummy see if we have the variant call with the same genome
 uniprot_kmers="/cluster/work/grlab/projects/TCGA/PanCanAtlas/tcga_immuno/uniprot/9mers_uniprot-human-UP000005640_9606.tsv"
+uniprot_kmers="/cluster/work/grlab_shared/TMP/prelotla/uniprot/9mers_uniprot-human-UP000005640_9606.tsv" #TODO change
 heter_code='0'
 kmer='9'
 
 #Note 
 #/cluster/work/grlab/projects/projects2021-immuno_peptides/results/TCGA_for_neoepitopes/TCGA_Breast_1102_results/splicing/spladder/genes_graph_${conf}.merge_graphs.count.hdf5 seems to be the realignement + reurun of spladder graph.
-for sample_type in TCGA_Breast_1102 TCGA_Ovarian_374; do # TCGA_All_Normals TCGA_Ovarian_374 TCGA_Breast_1102; do 
+for sample_type in TCGA_Ovarian_374 ; do #TCGA_Ovarian_374; do #TCGA_Breast_1102 TCGA_Ovarian_374; do # TCGA_All_Normals TCGA_Ovarian_374 TCGA_Breast_1102; do 
 	if [ "$sample_type" == "TCGA_Ovarian_374" ]; then  
 	    count_path=${basedir_splice}/TCGA_Ovarian_374_results/splicing/spladder/genes_graph_${conf}.merge_graphs.count.rechunked.hdf5
 	    splice_path=${basedir_splice}/TCGA_Ovarian_374_results/splicing/spladder/genes_graph_${conf}.merge_graphs.pickle  
