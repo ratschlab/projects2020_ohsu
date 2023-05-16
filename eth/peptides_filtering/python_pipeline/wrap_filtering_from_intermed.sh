264dc29bd90a75c2481482cc1d12ff618f1aa66e
lthp <laurie.prelot@inf.ethz.ch>
2023-05-16 14:13:56 +0200

    update bash scripts

--- eth/peptides_filtering/python_pipeline/wrap_filtering_from_intermed.sh
+++ eth/peptides_filtering/python_pipeline/wrap_filtering_from_intermed.sh
@@ -12,20 +12,22 @@ launch_script=send_filtering_from_intermediate.py
 
 
 # ----- ANALYSIS Parameters -----
-
+star_run='False'
 sample_type='brca'
-
+filtering_id='filters_15May_order_wany_cp_wAnnot'
 if [ "${sample_type}" == 'ov' ]; then
-base_cancer='/cluster/work/grlab/projects/projects2020_OHSU/peptides_generation/CANCER_eth/commit_c4dd02c_conf2_Frame_cap0_runs/TCGA_Ovarian_374'
-samples='TCGA-25-1319-01A-01R-1565-13.all TCGA-25-1313-01A-01R-1565-13.all TCGA-61-2008-01A-02R-1568-13.all TCGA-24-1431-01A-01R-1566-13.all TCGA-24-2298-01A-01R-1569-13.all'
-intermediate_output="${base_cancer}/filtering_intermediate/complete_cancer_candidates_order_r_complete.tsv.gzp"
-filtering_id='filters_15May_order_wany_cp_wAnnot'
-
+	base_cancer='/cluster/work/grlab/projects/projects2020_OHSU/peptides_generation/CANCER_eth/commit_c4dd02c_conf2_Frame_cap0_runs/TCGA_Ovarian_374'
+	samples='TCGA-25-1319-01A-01R-1565-13.all TCGA-25-1313-01A-01R-1565-13.all TCGA-61-2008-01A-02R-1568-13.all TCGA-24-1431-01A-01R-1566-13.all TCGA-24-2298-01A-01R-1569-13.all'
 elif [ "${sample_type}" == 'brca' ]; then
-base_cancer='/cluster/work/grlab/projects/projects2020_OHSU/peptides_generation/CANCER_eth/commit_c4dd02c_conf2_Frame_cap0_runs/TCGA_Breast_1102'
-samples='TCGA-AO-A0JM-01A-21R-A056-07.all TCGA-C8-A12P-01A-11R-A115-07.all TCGA-BH-A18V-01A-11R-A12D-07.all TCGA-A2-A0D2-01A-21R-A034-07.all TCGA-A2-A0SX-01A-12R-A084-07.all'
-intermediate_output="${base_cancer}/filtering_intermediate/complete_cancer_candidates_order_r_complete.tsv.gz"
-filtering_id='filters_15May_order_wany_cp_wAnnot'
+	base_cancer='/cluster/work/grlab/projects/projects2020_OHSU/peptides_generation/CANCER_eth/commit_c4dd02c_conf2_Frame_cap0_runs/TCGA_Breast_1102'
+	samples='TCGA-AO-A0JM-01A-21R-A056-07.all TCGA-C8-A12P-01A-11R-A115-07.all TCGA-BH-A18V-01A-11R-A12D-07.all TCGA-A2-A0D2-01A-21R-A034-07.all TCGA-A2-A0SX-01A-12R-A084-07.all'
+fi
+if [ "${star_run}" == 'False' ] ; then
+        intermediate_output="${base_cancer}/filtering_intermediate/complete_cancer_candidates_order_r_complete.tsv.gz"
+	filtering_id_=${filtering_id}
+else
+	intermediate_output="${base_cancer}/filtering_intermediate/complete_cancer_candidates_order_r_complete.STAR_GTEx.tsv.gz"
+	filtering_id_=${filtering_id}_STAR
 fi
 
 Threshold_target='0.0'
@@ -48,7 +50,7 @@ echo "#SBATCH -J ${job_name}" >> ${launch_script}
 echo "#SBATCH --time=${hours}:00:00" >> ${launch_script}
 echo "#SBATCH --cpus-per-task=${cpus}" >> ${launch_script}
 echo "#SBATCH --mem=${mem}" >> ${launch_script}
-cmd="python ./filtering_from_intermediate.py --basedir ${base_cancer} --intermediate-output ${intermediate_output} --filtering-id ${filtering_id} --target-samples ${samples} --Threshold-target ${Threshold_target} --Threshold-cancer-cohort ${Threshold_cancer_cohort} --N-samples-cancer ${N_samples_cancer} --Threshold-normal-cohort ${Threshold_normal_cohort} --N-samples-normal ${N_samples_normal}"
+cmd="python ./filtering_from_intermediate.py --basedir ${base_cancer} --intermediate-output ${intermediate_output} --filtering-id ${filtering_id_} --target-samples ${samples} --Threshold-target ${Threshold_target} --Threshold-cancer-cohort ${Threshold_cancer_cohort} --N-samples-cancer ${N_samples_cancer} --Threshold-normal-cohort ${Threshold_normal_cohort} --N-samples-normal ${N_samples_normal}"
 echo $cmd >>  ${launch_script}
 
 echo "Output to ${log_file}"
