7164e0f9af69774c1c4590e3e4ce2151bce2dfd5
lthp <laurie.prelot@inf.ethz.ch>
2023-12-07 17:50:40 +0100

    proteomics ran

---
+++ pnnl/20230714-tide-search/script_search.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+crux_home=$1
+overwrite=$2
+output_dir=$3
+ms_partition=$4
+database=$5
+
+bashCommand="${crux_home} tide-search --overwrite T --precursor-window 40 --concat T --top-match 1000000000 --num-threads 1 --output-dir ${output_dir} ${ms_partition} ${database}"
+echo $bashCommand
---
+++ pnnl/20230714-tide-search/tide-search-rewriten.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+crux_home=~/util/crux-4.1.Linux.x86_64/bin/crux
+gitfolder=${PWD}
+
+fa_eth=/cluster/work/grlab/projects/projects2020_OHSU/share_OHUS_PNLL/ETH_Oct2023_data/ETH_fasta_list.txt
+fa_ohsu=/cluster/work/grlab/projects/projects2020_OHSU/share_OHUS_PNLL/OHSU_Oct2023_data/OHSU_fasta_list.txt
+ms_datadir=/cluster/work/grlab/projects/TCGA/PanCanAtlas/peptides_neoantigen/analysis_pancan/ccell_rerun_2018/data/cptac
+
+basedir=/cluster/work/grlab/projects/projects2020_OHSU/proteomics
+outdir=${basedir}/tide_search_joint
+mkdir -p ${outdir}
+
+overwrite='T'
+while read f;
+do
+    	sample=$(basename $f | cut -d '_' -f2 | cut -d '-' -f1-3 )
+	
+	database=${basedir}/neighbors_joint/${sample}/tide-indicies/final # finalDb.fasta # Do we need the indices there concatenated....?
+
+	for partition in ${ms_datadir}/${sample}/*mzML*; do 
+		part_name=$(basename ${partition}| sed 's/\.mzML\.gz//g')
+		searchdir=${outdir}/${sample}/${part_name}
+		mkdir -p ${searchdir}
+		cd ${searchdir}
+		echo $f
+		echo "results in ${outdir}"
+		sh ${gitfolder}/script_search.sh ${crux_home} ${overwrite} ${searchdir} ${partition} ${database}
+	done
+done < ${fa_eth} #same fa_eth works too
