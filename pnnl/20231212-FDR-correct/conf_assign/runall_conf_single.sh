c0a6a52d93d29728f3a88b63e74c6d89762c148c
lthp <laurie.prelot@inf.ethz.ch>
2023-12-22 15:39:54 +0100

    new year checkpoint

--- pnnl/20231212-FDR-correct/conf_assign/runall_conf_single.sh
+++ pnnl/20231212-FDR-correct/conf_assign/runall_conf_single.sh
@@ -3,15 +3,22 @@
 gitfolder=${PWD}
 crux_home=/cluster/home/prelotla/util/crux-4.1.Linux.x86_64/bin/crux
 overwrite='T'
+pipeline=ETH #ETH #Two pipelines to run
 
-experiment_list='/cluster/work/grlab/projects/projects2020_OHSU/share_OHUS_PNLL/OHSU_Oct2023_data/OHSU_experiments_per_peptides_list.txt'
-exp_res='/cluster/work/grlab/projects/projects2020_OHSU/proteomics/OHSU/*/tide_search_per_experiment'
-out_folder='/cluster/work/grlab/projects/projects2020_OHSU/proteomics/OHSU'
+if [[ "${pipeline}" == 'OHSU' ]]; then
+	experiment_list='/cluster/work/grlab/projects/projects2020_OHSU/share_OHUS_PNLL/OHSU_Oct2023_data/OHSU_experiments_per_peptides_list.txt'
+elif [[ "${pipeline}" == 'ETH' ]]; then
+	experiment_list='/cluster/work/grlab/projects/projects2020_OHSU/share_OHUS_PNLL/ETH_Oct2023_data/ETH_experiments_per_peptides_list.txt'
+fi
+echo "experiment file for $pipeline is: $experiment_list"
+
+exp_res="/cluster/work/grlab/projects/projects2020_OHSU/proteomics/${pipeline}/*/tide_search_per_experiment"
+out_folder="/cluster/work/grlab/projects/projects2020_OHSU/proteomics/${pipeline}"
 
 for sample_out in $(ls -d ${exp_res}); do 
 	sample=$(echo ${sample_out} | cut -f9 -d '/')
 	echo $sample
-	outfolder="/cluster/work/grlab/projects/projects2020_OHSU/proteomics/OHSU/${sample}/assign_conf_per_experiment"
+	outfolder="/cluster/work/grlab/projects/projects2020_OHSU/proteomics/${pipeline}/${sample}/assign_conf_per_experiment"
         echo ${outfolder}
 	mkdir -p ${outfolder}
     for experiment in ${sample_out}/*; do
@@ -23,11 +30,3 @@ for sample_out in $(ls -d ${exp_res}); do
    done
 done
 
-
-
-#experiment_list='/cluster/work/grlab/projects/projects2020_OHSU/share_OHUS_PNLL/ETH_Oct2023_data/ETH_experiments_per_peptides_list.txt'
-#search_res='/cluster/work/grlab/projects/projects2020_OHSU/proteomics/ETH/*/tide_search'
-#out_folder='/cluster/work/grlab/projects/projects2020_OHSU/proteomics/ETH'
-#cd ${out_folder}
-#echo ${out_folder}
-#sbatch ${gitfolder}/script_psm.sh ${gitfolder} ${experiment_list} "${search_res}" ${out_folder}
