d79309175c9bb4da26f96f6050c5d505fe2bd261
lthp <laurie.prelot@inf.ethz.ch>
2024-01-16 16:40:41 +0100

    started processing proteomics results

--- pnnl/20231212-FDR-correct/POOL_3_FDR_extract/runall_split.sh
+++ pnnl/20231212-FDR-correct/POOL_3_FDR_extract/runall_split.sh
@@ -2,33 +2,33 @@
 
 gitfolder=${PWD}
 
-### OHSU POOL
-experiment_list='/cluster/work/grlab/projects/projects2020_OHSU/share_OHUS_PNLL/OHSU_Oct2023_data/OHSU_experiments_per_peptides_list.txt'
-search_res='/cluster/work/grlab/projects/projects2020_OHSU/proteomics/OHSU/*/tide_search'
-out_folder='/cluster/work/grlab/projects/projects2020_OHSU/proteomics/OHSU'
-sample_pos=8
-make_sub_folder='T'
-mkdir -p ${out_folder}
-cd ${out_folder}
-echo ${out_folder}
-sbatch ${gitfolder}/script_split.sh ${gitfolder} ${experiment_list} "${search_res}" ${out_folder} ${sample_pos} ${make_sub_folder}
-
-
-### ETH POOL
-experiment_list='/cluster/work/grlab/projects/projects2020_OHSU/share_OHUS_PNLL/ETH_Oct2023_data/ETH_experiments_per_peptides_list.txt'
-search_res='/cluster/work/grlab/projects/projects2020_OHSU/proteomics/ETH/*/tide_search'
-out_folder='/cluster/work/grlab/projects/projects2020_OHSU/proteomics/ETH'
-sample_pos=8
-make_sub_folder='T'
-mkdir -p ${out_folder}
-cd ${out_folder}
-echo ${out_folder}
-sbatch ${gitfolder}/script_split.sh ${gitfolder} ${experiment_list} "${search_res}" ${out_folder} ${sample_pos} ${make_sub_folder}
-
+#### OHSU POOL
+#experiment_list='/cluster/work/grlab/projects/projects2020_OHSU/share_OHUS_PNLL/OHSU_Oct2023_data/OHSU_experiments_per_peptides_list.txt'
+#search_res='/cluster/work/grlab/projects/projects2020_OHSU/proteomics/OHSU/*/tide_search'
+#out_folder='/cluster/work/grlab/projects/projects2020_OHSU/proteomics/OHSU'
+#sample_pos=8
+#make_sub_folder='T'
+#mkdir -p ${out_folder}
+#cd ${out_folder}
+#echo ${out_folder}
+#sbatch ${gitfolder}/script_split.sh ${gitfolder} ${experiment_list} "${search_res}" ${out_folder} ${sample_pos} ${make_sub_folder}
+#
+#
+#### ETH POOL
+#experiment_list='/cluster/work/grlab/projects/projects2020_OHSU/share_OHUS_PNLL/ETH_Oct2023_data/ETH_experiments_per_peptides_list.txt'
+#search_res='/cluster/work/grlab/projects/projects2020_OHSU/proteomics/ETH/*/tide_search'
+#out_folder='/cluster/work/grlab/projects/projects2020_OHSU/proteomics/ETH'
+#sample_pos=8
+#make_sub_folder='T'
+#mkdir -p ${out_folder}
+#cd ${out_folder}
+#echo ${out_folder}
+#sbatch ${gitfolder}/script_split.sh ${gitfolder} ${experiment_list} "${search_res}" ${out_folder} ${sample_pos} ${make_sub_folder}
+#
 
 ### UNION EXTRACT OHSU EXP
 experiment_list='/cluster/work/grlab/projects/projects2020_OHSU/share_OHUS_PNLL/OHSU_Oct2023_data/OHSU_experiments_per_peptides_list.txt'
-search_res='/cluster/work/grlab/projects/projects2020_OHSU/proteomics/OHSU/*/tide_search'
+search_res='/cluster/work/grlab/projects/projects2020_OHSU/proteomics/tide_search_joint/*'
 out_folder='/cluster/work/grlab/projects/projects2020_OHSU/proteomics/assign_conf_joint_to_OHSU'
 sample_pos=8
 make_sub_folder='F'
@@ -39,7 +39,7 @@ sbatch ${gitfolder}/script_split.sh ${gitfolder} ${experiment_list} "${search_re
 
 ### INUIN EXTRACT ETH EXP
 experiment_list='/cluster/work/grlab/projects/projects2020_OHSU/share_OHUS_PNLL/ETH_Oct2023_data/ETH_experiments_per_peptides_list.txt'
-search_res='/cluster/work/grlab/projects/projects2020_OHSU/proteomics/ETH/*/tide_search'
+search_res='/cluster/work/grlab/projects/projects2020_OHSU/proteomics/tide_search_joint/*'
 out_folder='/cluster/work/grlab/projects/projects2020_OHSU/proteomics/assign_conf_joint_to_ETH'
 sample_pos=8
 make_sub_folder='F'
