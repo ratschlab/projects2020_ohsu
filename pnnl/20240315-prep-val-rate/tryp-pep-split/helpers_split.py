239cf0da6006aaa9680db08f9cbe6edf04f17ddb
lthp <laurie.prelot@inf.ethz.ch>
2024-03-15 14:58:19 +0100

    refactoring prior to tryptic peptide split

---
+++ pnnl/20240315-prep-val-rate/tryp-pep-split/helpers_split.py
@@ -0,0 +1,86 @@
+import os 
+from pathlib import Path
+import pandas as pd
+from collections import defaultdict
+import glob
+import timeit
+import numpy as np
+
+
+def reader_FDR_results(search_out_folder, sample_search_out_folder):
+    search_res = dict()
+    for path in glob.glob(search_out_folder):
+        sample = path.split('/')[sample_search_out_folder]
+        search_res[sample] = path 
+    return search_res
+
+def reader_experiments(list_experiments):
+    '''Read files with path lists'''
+    with open(list_experiments, 'r') as f:
+        path_dict = {}
+        for i in f.readlines():
+            sample_name = os.path.basename(i.strip()).split('_')[1]
+            sample_short = '-'.join(sample_name.split('-')[0:3])
+            path_dict[sample_short] = i.strip()
+    return path_dict
+
+
+def experiments_maps(path):
+    '''Extract experiment maps'''
+    df = pd.read_csv(path, sep = '\t')
+    id_to_pep = {}
+    id_to_exp = {}
+    exp_to_id = defaultdict(list)
+    for i, row in df.iterrows():
+        id_to_pep[row['peptide_id']] = row['peptide_sequence']
+        id_to_exp[row['peptide_id']] = row['experiment_ids'].split(';')
+
+    for k, v in id_to_exp.items():
+        for ID in v:
+            exp_to_id[ID].append(k)
+    return id_to_pep, id_to_exp, exp_to_id
+
+
+def search_result_rows(df_search):
+    id_to_row = defaultdict(list)
+    for i, idx in enumerate(df_search['protein id']):
+        if idx is np.nan:
+            print('ERROR: Search not successful on all fractions of sample. Please RERUN')
+        for name_ in idx.split(','):
+            if 'pepID' not in name_:
+                continue
+            pep_ix = int(name_.split('-')[1].split('(')[0])
+            id_to_row[pep_ix].append(i)
+    return id_to_row
+
+
+def select_search_result(id_to_exp, id_to_SearchRow):
+    select_rows = defaultdict(set)
+    for pep_idx, exp_list in id_to_exp.items():
+        for experiment in exp_list:
+            peptide_rows = id_to_SearchRow[pep_idx]
+            if peptide_rows:
+                select_rows[experiment].update(peptide_rows)
+    return select_rows
+
+
+def reconstruct_experiment_FDR(select_rows_pipeline, df_search, save_folder, sample, create_sample_subfolder):
+    '''Selects all the rows from the initial experiment
+    Save'''
+    df_search_i = df_search.reset_index()
+    for experiment_id in select_rows_pipeline:
+        print(f'.....{experiment_id}')
+
+        df_experiment = df_search_i.loc[list(select_rows_pipeline[experiment_id])]
+        df_experiment = df_experiment.drop_duplicates()
+        
+
+        if create_sample_subfolder:
+            path_save = os.path.join(save_folder, sample, create_sample_subfolder) 
+        else:
+            path_save = os.path.join(save_folder, sample)
+
+        Path(path_save).mkdir(parents=True, exist_ok=True)
+        path_save = os.path.join(path_save, f'tsearch-{experiment_id}.txt')
+        print(path_save)
+        df_experiment.to_csv(path_save, sep = '\t', index=None)
---
+++ pnnl/20240315-prep-val-rate/tryp-pep-split/script_split.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+#SBATCH --job-name=FDRsplit
+#SBATCH --output=FDRsplit.out
+#SBATCH --cpus-per-task=1
+#SBATCH --nodes=1
+#SBATCH --time=04:00:00
+#SBATCH --mem-per-cpu=50GB
+
+script_home=$1
+list_experiments=$2
+search_res=$3
+out_folder=$4
+sample_pos=$5
+make_sub_folder=$6
+
+if [ -z "$make_sub_folder" ]; then
+	python ${script_home}/FDR_to_experiments.py --list-experiments "${list_experiments}" --search-out-folder "${search_res}" --save-folder "${out_folder}" --sample-search-out-folder ${sample_pos}
+	echo "python ${script_home}/FDR_to_experiments.py --list-experiments "${list_experiments}" --search-out-folder "${search_res}" --save-folder "${out_folder}" --sample-search-out-folder ${sample_pos}" >> ${script_home}/run_example.sh
+else
+	python ${script_home}/FDR_to_experiments.py --list-experiments "${list_experiments}" --search-out-folder "${search_res}" --save-folder "${out_folder}" --sample-search-out-folder ${sample_pos} --create-sample-subfolder ${make_sub_folder}
+	echo "python ${script_home}/FDR_to_experiments.py --list-experiments "${list_experiments}" --search-out-folder "${search_res}" --save-folder "${out_folder}" --sample-search-out-folder ${sample_pos} --create-sample-subfolder ${make_sub_folder}" >> ${script_home}/run_example.sh
+fi
