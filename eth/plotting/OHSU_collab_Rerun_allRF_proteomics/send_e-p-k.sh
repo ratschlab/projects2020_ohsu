045e0a43c60563988182b5563f59ffa5563ec6ef
lthp <laurie.prelot@inf.ethz.ch>
2024-03-20 18:04:54 +0100

    checkpoint

--- eth/plotting/OHSU_collab_Rerun_allRF_proteomics/send_e-p-k.sh
+++ eth/plotting/OHSU_collab_Rerun_allRF_proteomics/send_e-p-k.sh
@@ -5,7 +5,7 @@ save_folder='/cluster/work/grlab/projects/projects2020_OHSU/peptides_generation/
 
 
 for MS_FDR in '_crema' '_crux'; do 
-    for MS_strategy in 'joint' 'joint' 'single'; do
+    for MS_strategy in 'pool' 'joint' 'single'; do
     	mkdir -p ${save_folder}/run_${MS_strategy}${MS_FDR}
 	cd ${save_folder}/run_${MS_strategy}${MS_FDR}
         sbatch ${script_folder}/run_extract-peptides-to-kmer.sh ${MS_FDR} ${MS_strategy} 
